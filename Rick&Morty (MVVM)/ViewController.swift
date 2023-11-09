2e37f76eed8284f4cbee36fc8c0d5e412c0a704e
Alvaro <alvaro.gomezgarcia@gruposantander.com>
2023-11-09 17:18:35 +0100

    Initial Commit

---
+++ Rick&Morty (MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty (MVVM)
+//
+//  Created by Alvaro Gomez Garcia on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
